ee46365db21f4860ba944713e20f7c0050ae7a5e
ayou <550244300@qq.com>
2018-04-03 16:56:11 +0800

    copy代码过来 Caffeine做一级缓存 redis做二级缓存，基于redis的分布式锁。

---
+++ ayoue-common/ayoue-common-cache/src/main/java/io/ayoue/common/cache/listener/RedisMessageListener.java
@@ -0,0 +1,75 @@
+package io.ayoue.common.cache.listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+
+import io.ayoue.common.cache.enums.ChannelTopicEnum;
+import io.ayoue.common.cache.layering.LayeringCache;
+import io.ayoue.common.cache.redis.serializer.KryoRedisSerializer;
+
+/**
+ * redis消息的订阅者
+ *
+ * @author yuhao.wang
+ */
+@Component
+public class RedisMessageListener extends MessageListenerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(RedisPublisher.class);
+
+    @Autowired
+    CacheManager cacheManager;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        super.onMessage(message, pattern);
+        ChannelTopicEnum channelTopic = ChannelTopicEnum.getChannelTopicEnum(new String(message.getChannel()));
+        logger.info("redis消息订阅者接收到频道【{}】发布的消息。消息内容：{}", channelTopic.getChannelTopicStr(), message.toString().getBytes());
+        // 解析订阅发布的信息，获取缓存的名称和缓存的key
+        KryoRedisSerializer<Object> kryoRedisSerializer = new KryoRedisSerializer<>(Object.class);
+        //String ms = new String(kryoRedisSerializer.deserialize(message.getBody()));
+        String a = (String) kryoRedisSerializer.deserialize(message.getBody());
+        String ms = new String(message.getBody());
+        logger.info("转换后{}", ms );
+        @SuppressWarnings("unchecked")
+		Map<String, Object> map = JSON.parseObject(a, HashMap.class);
+        String cacheName = (String) map.get("cacheName");
+        Object key = map.get("key");
+
+        // 根据缓存名称获取多级缓存
+        Cache cache = cacheManager.getCache(cacheName);
+
+        // 判断缓存是否是多级缓存
+        if (cache != null && cache instanceof LayeringCache) {
+            switch (channelTopic) {
+                case REDIS_CACHE_DELETE_TOPIC:
+                    // 获取一级缓存，并删除一级缓存数据
+                    ((LayeringCache) cache).getFirstCache().evict(key);
+                    logger.info("删除一级缓存{}数据,key:{}", cacheName, key.toString().getBytes());
+                    break;
+
+                case REDIS_CACHE_CLEAR_TOPIC:
+                    // 获取一级缓存，并删除一级缓存数据
+                    ((LayeringCache) cache).getFirstCache().clear();
+                    logger.info("清除一级缓存{}数据", cacheName);
+                    break;
+
+                default:
+                    logger.info("接收到没有定义的订阅消息频道数据");
+                    break;
+            }
+
+        }
+    }
+
+}
